916eb747e1f49049e40c47088d61855da8afba20
Manish Sharma <manish.sharma@ggktech.local>
2019-01-08 16:55:17 +0530

    Login UI

--- ui-zuul/src/main/web-app/src/app/material.ts
+++ ui-zuul/src/main/web-app/src/app/material.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+      MatInputModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+      MatInputModule
   ]
 })
 export class MaterialModules { }
