39463ec3ae2d348bc8edb3f83abdcf6f9329f281
skrudra <skrudra12@gmail.com>
2019-03-26 15:44:34 +0530

    Test case setup

--- login-registration/src/test/java/com/microservice/loginregistration/LoginRegistrationApplicationTests.java
+++ login-registration/src/test/java/com/microservice/loginregistration/LoginRegistrationApplicationTests.java
@@ -2,11 +2,14 @@ package com.microservice.loginregistration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class LoginRegistrationApplicationTests {
 
 	@Test
