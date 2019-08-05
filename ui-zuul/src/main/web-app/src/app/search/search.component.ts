6af3ecea1c6dec2426b41c49fba9e5fdf65fa0cb
Manish Sharma <manish.sharma@ggktech.com>
2019-08-05 18:11:46 +0530

    Autocomplete frontend and added extra profiles in db

--- ui-zuul/src/main/web-app/src/app/search/search.component.ts
+++ ui-zuul/src/main/web-app/src/app/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ProfileService } from './../services/profile.service';
+import { Profile } from './../models/profile';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, tap, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private profileService: ProfileService,
+  ) { }
+    
+  searchName = new FormControl();
+  profiles: Observable<Profile[]>;
 
   ngOnInit() {
-  }
+      this.profiles = this.searchName.valueChanges
+                        .pipe(  debounceTime(400),
+                                distinctUntilChanged(),
+                                filter((name) => name),
+                                switchMap((name) => 
+                                    this.profileService.getProfilesFromKeyword(name)
+                                ));
+  }  
 
 }
