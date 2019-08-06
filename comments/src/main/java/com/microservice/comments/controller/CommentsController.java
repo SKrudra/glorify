4d439a33f6e6a79d04ed12ed1b60939d21744b75
Manish Sharma <manish.sharma@ggktech.com>
2019-08-06 17:10:46 +0530

    Comments backend to fetch all comments

--- comments/src/main/java/com/microservice/comments/controller/CommentsController.java
+++ comments/src/main/java/com/microservice/comments/controller/CommentsController.java
@@ -1,17 +1,26 @@
 package com.microservice.comments.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.microservice.comments.repository.CommentsRepository;
+import com.microservice.comments.entity.Comment;
+import com.microservice.comments.service.CommentsService;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentsController {
 	
 	@Autowired
-	private CommentsRepository commentRepository;
+	private CommentsService commentsService;
 	
+	@GetMapping(value="/allon/{userId}")
+	List<Comment> getAllCommentsOn(@PathVariable Long userId) {
+		return commentsService.getAllCommentsOn(userId);
+	}
 	
 }
