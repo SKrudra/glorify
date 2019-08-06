303133067b0028d0710c1fda55288f635cf86d5a
Manish Sharma <manish.sharma@ggktech.com>
2019-08-06 16:47:31 +0530

    Added comments in db and deleted unnecessary files in comments microservice

--- comments/src/main/java/com/microservice/comments/controller/CommentsController.java
+++ comments/src/main/java/com/microservice/comments/controller/CommentsController.java
@@ -1,62 +1,17 @@
 package com.microservice.comments.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.microservice.comments.repository.CommentRepository;
-import com.microservice.comments.model.Comments;
+import com.microservice.comments.repository.CommentsRepository;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentsController {
 	
 	@Autowired
-	CommentRepository commentRepository;
+	private CommentsRepository commentRepository;
 	
-	@PostMapping("/createComment")
-	public ResponseEntity<Comments> createComment(@RequestBody Comments comment) {		
-		return new ResponseEntity<Comments>(commentRepository.save(comment), HttpStatus.OK);
-	}
 	
-	@GetMapping("getComments")
-	public List<Comments> getComment(){
-		return  commentRepository.findAll();
-	}
-	
-	@GetMapping("getCommentsByCommentTo")
-	public List<Comments> getCommentsByCommentTo(@RequestBody String commentToId){
-		return  commentRepository.findByCommentTo(commentToId);
-	}
-	
-	@GetMapping("getCommentsByCommenter")
-	public List<Comments> getCommentsByCommentBy(@RequestBody String commentById){
-		return  commentRepository.findByCommentTo(commentById);
-	}
-	
-	@GetMapping("getComment")
-	public Optional<Comments> getComment(@RequestBody String commentId){
-		return commentRepository.findById(commentId);
-	}
-	
-
-	@PutMapping("updateComment")
-	public Comments updateComment(@RequestBody Comments comment){
-		return commentRepository.save(comment);
-	}
-
-	@DeleteMapping("deleteComment")
-	public void deleteComment(@RequestBody String commentId){
-		commentRepository.deleteById(commentId);
-	}
 }
---
+++ comments/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into comments (ID, COMMENTED_BY, COMMENTED_ON, DESCRIPTION, IS_PRIVATE, UPVOTES, DOWNVOTES, PARENT_ID)
+values (1,2,1,"You are great, my lord", false, 3,0,NULL),
+(2,4,1,"I'm sure you're gonna be famous bro", false, 5,0, NULL),
+(3,5,1,"We have just two words for you -> The Best", false, 2,0, NULL),
+(4,1,4,"Tum se na ho payega", false, 100,1, NULL);
