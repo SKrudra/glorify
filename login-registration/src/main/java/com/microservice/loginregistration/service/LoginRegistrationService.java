5e0b108aa7a6f3ea145109edbd0e6be6bb3ada9c
Manish Sharma <manish.sharma@ggktech.com>
2019-12-25 14:13:00 +0530

    Some changes

--- login-registration/src/main/java/com/microservice/loginregistration/service/LoginRegistrationService.java
+++ login-registration/src/main/java/com/microservice/loginregistration/service/LoginRegistrationService.java
@@ -2,21 +2,30 @@ package com.microservice.loginregistration.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.microservice.loginregistration.entity.LoginRegistrationEntity;
 import com.microservice.loginregistration.repository.LoginRegistrationRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class LoginRegistrationService {
+	
+	Logger log = LoggerFactory.getLogger(LoginRegistrationService.class);
 	
 	@Autowired
 	private LoginRegistrationRepository loginRegistrationRepository;
 	
 	public Long validateCredentials(String email, String password) {
 		Optional<LoginRegistrationEntity> opEntity =  loginRegistrationRepository.findByEmail(email);
+		
 		if(opEntity.isPresent()) {
+			log.trace(opEntity.get().toString());
 			LoginRegistrationEntity credentials = opEntity.get();
 			if(password.equals(credentials.getPassword())) {
 				return credentials.getId();
